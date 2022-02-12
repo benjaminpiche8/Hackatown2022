e36df10f8966ed235b785eaf56501f541477d052
benjaminpiche8 <benjamin.piche@polymtl.ca>
2022-02-12 13:50:36 -0500

    added icons folder

--- meteo.py
+++ meteo.py
@@ -2,7 +2,9 @@ import json
 import requests
 from PIL import Image
 
-def current_meteo_type() :
+### Current conditions ###
+
+def get_current_icon() :
     with open('database\database_current.json', 'r') as outfile :
         data = json.load(outfile)
         url = data['condition']['icon']
@@ -10,5 +12,44 @@ def current_meteo_type() :
         image.show()
         return image
 
+def get_current_condition() :
+    with open('database\database_current.json', 'r') as outfile :
+        data = json.load(outfile)
+        return data['condition']['text']
+
+def get_current_raw_temp(system) :
+    with open('database\database_current.json', 'r') as outfile :
+        data = json.load(outfile)
+        if system == 'c' :
+            return data['temp_c']
+        else :
+            return data['temp_f']
+
+def get_current_feelslike_temp(system) :
+    with open('database\database_current.json', 'r') as outfile :
+        data = json.load(outfile)
+        if system == 'c' :
+            return data['feelslike_c']
+        else :
+            return data['feelslike_f']
+
+
+### Forecast conditions ###
+
+def get_forecast_icon() :
+    with open('database\database_forecast.json', 'r') as outfile :
+        data = json.load(outfile)
+        values = list(data.values())
+        for sublist in values :
+            for dict in sublist :
+                print(dict, '\n', '\n')
+        #    print(date, '\n', '\n')
+        #print(type(items), '\n')
+        #url = data['condition']['icon']
+        #image = Image.open(requests.get('https:' + url, stream = True).raw)
+        #image.show()
+        #return image
+
 if __name__ == "__main__" :
-    current_meteo_type()
+    # print(type(get_forecast_icon()), get_forecast_icon())
+    get_forecast_icon()
