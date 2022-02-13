81ee737238bf9ff2b7f052b1965e46ffca1878bc
benjaminpiche8 <benjamin.piche@polymtl.ca>
2022-02-12 20:42:45 -0500

    get_clothing_icon function added

--- outfit.py
+++ outfit.py
@@ -2,6 +2,8 @@ import meteo as mt
 from datetime import date
 import random
 
+def get_clothing_icon(item) : # string parameter with a clothing item
+    return 'icons_clothes/' + str(item)
 
 def short_term_recommandation_top(sex = 'm') :
     c_raw_temp = mt.get_current_raw_temp('c')
